10a13f396b0c841fe5943c47889c6a383c6515b8
Sherry Li <165175443+sherrylxf@users.noreply.github.com>
2024-12-04 20:25:40 +0800

    Update Monster.h

--- Classes/Monster.h
+++ Classes/Monster.h
@@ -1,5 +1,26 @@
 #pragma once
-class Monster
-{
+#include "cocos2d.h"
+#include "HelloWorldScene.h"
+
+class Monster_value {
+public:
+	int type;//怪物的种类
+	int HP;//生命值
+	int full_HP;//满血
+	int ATK;//攻击值
+
+	int Experience;//获得的金币
+
+	int Speed;//移动速度
+	int move_Count;//移动步数
+	double poison_Time = 0;//中毒时间
+	int poison_Speed;//中毒的移动速度
+
+	cocos2d::Sprite* enemy_picture; //障碍贴图
 };
 
+class Monster : public cocos2d::Sprite{
+private:
+	Monster_value monster;
+public:	
+};
