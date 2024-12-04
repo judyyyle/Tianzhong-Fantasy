6b0e498392b1965894c2b747f1089c93fe2bfca7
judyyyle <judyyy2023@gmail.com>
2024-12-04 09:25:30 +0800

    Add files via upload

---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,22 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::Scene
+{
+public:
+    // 创建场景
+    static cocos2d::Scene* createScene();
+
+    // 初始化
+    virtual bool init();
+    
+    // a selector callback（一个选择回调函数）
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(HelloWorld);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
