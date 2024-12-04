0a539fc66d3f3683ff3418b469ab14cf9421c5d2
Sherry Li <165175443+sherrylxf@users.noreply.github.com>
2024-12-04 22:23:08 +0800

    Update Monster.cpp

--- Classes/Monster.cpp
+++ Classes/Monster.cpp
@@ -1,1 +1,16 @@
 #include "Monster.h"
+#include "HelloWorldScene.h"
+#include <cmath>
+#include <vector>
+#include<algorithm>
+
+#include "Tower.h"
+
+extern int carrot_HP;//萝卜血量
+
+extern int monster_number;//怪物总数
+extern int barrier_number;//障碍总数
+extern std::vector<Monster*>monster;
+extern std::vector<Monster*>barrier;
+
+extern Monster* destination;//怪物位置
