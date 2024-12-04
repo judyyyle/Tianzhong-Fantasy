b09b4ecaa797d9e56de5328d671d28bd83584bcc
kdb_zcr <3380782523@qq.com>
2024-12-04 23:06:29 +0800

    Update HelloWorldScene.h
    添加私有数据成员 （但还未使用）

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
@@ -6,17 +30,21 @@
 class HelloWorld : public cocos2d::Scene
 {
 public:
-    // ¥¥Ω®≥°æ∞
     static cocos2d::Scene* createScene();
 
-    // ≥ı ºªØ
     virtual bool init();
     
-    // a selector callback£®“ª∏ˆ—°‘Òªÿµ˜∫Ø ˝£©
+    // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+
+  private:
+        cocos2d::MenuItemImage* startItem;  // ÂºÄÂßãÊåâÈíÆ
+        cocos2d::MenuItemImage* exitItem;   // ÈÄÄÂá∫ÊåâÈíÆ
+        cocos2d::MenuItemImage* adventureItem; // ÂéªÂÜíÈô©Ê®°ÂºèÊåâÈíÆ
+        cocos2d::MenuItemImage* bossItem;   // Âéª Boss Ê®°ÂºèÊåâÈíÆ
 };
 
 #endif // __HELLOWORLD_SCENE_H__
