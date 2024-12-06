fec9cce614cbd2fcd2fceefa055595751b4ec7b0
Sherry Li <165175443+sherrylxf@users.noreply.github.com>
2024-12-07 00:42:27 +0800

    更新怪物、障碍的信息初始化

--- Classes/Monster.cpp
+++ Classes/Monster.cpp
@@ -5,6 +5,7 @@
 #include<algorithm>
 
 #include "Tower.h"
+USING_NS_CC;
 
 extern int carrot_HP;//萝卜血量
 
@@ -14,3 +15,31 @@ extern std::vector<Monster*>monster;
 extern std::vector<Monster*>barrier;
 
 extern Monster* destination;//怪物位置
+
+Sprite* Monster::createSprite() {
+	return Monster::create();
+}
+
+//不同怪物、障碍的信息初始化
+void Monster::initType(int stage) {
+	monster.type = stage;
+	std::string picture1[] = { "normal01-2.PNG","fast01-2.PNG","huge01-2.PNG","One1.PNG","One2.PNG","Two1.PNG","Two2.PNG","Four1.PNG","Four2.PNG" };
+	std::string picture2[] = { "normal01-1.PNG","fast01-1.PNG","huge01-1.PNG" };
+
+
+	Vector<SpriteFrame*> monster;
+
+	switch (stage) {
+	case 0: //正常怪物
+		monster.pushBack(SpriteFrame::create(picture1[0], Rect(0, 0, 68, 87)));
+		monster.pushBack(SpriteFrame::create(picture2[0], Rect(0, 0, 75, 74)));
+		// 用this指针指定赋值成员变量
+		this->monster.full_HP = MONSTER_NORMAL_HP;
+		this->monster.HP = MONSTER_NORMAL_HP;
+		this->monster.ATK = 1;
+		this->monster.Speed = MONSTER_NORMAL_SPEED;
+		this->monster.Coin = MONSTER_COIN_NORMAL;
+
+		break;
+	}
+}
