65f69ac502a28188fb0ead8992dcc3222fdf766d
judyyyle <judyyy2023@gmail.com>
2024-12-09 17:56:53 +0800

    Update HelloWorldScene.h

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -40,24 +40,20 @@ struct array {
     int col;
 };
 
-struct vec2 {
-    float x;
-    float y;
-};
-
-vec2 array_to_vec2(int row, int col) {
-    vec2 vec;
+static Vec2 array_to_vec2(int row,int col) { //返回Vec2类型，即世界坐标
+    Vec2 vec;
     vec.x = 64 + 128 * col;
     vec.y = 1024 - 64 - 128 * row;
     return vec;
 }
 
-array vec2_to_array(vec2 vec) {
+array vec2_to_array(Vec2 vec) { //返回array类型，即数组
     array arr;
     arr.row = 8 - 0.5 - static_cast<int>((vec.y / 128));
     arr.col = static_cast<int>((vec.x / 128));
     return arr;
 }
+
 
 class HelloWorld : public cocos2d::Scene
 {
