d614b4f0a11e972ad1de68583115fe87c698a411
kdb_zcr <3380782523@qq.com>
2024-12-20 12:41:03 +0800

    Update GamePlayScene.h

--- Classes/GamePlayScene.h
+++ Classes/GamePlayScene.h
@@ -1,47 +1,52 @@
 #pragma once
+
 #ifndef __Game_Play_Scene_H__
-#define  __Game_Play_Scene_H__
+#define __Game_Play_Scene_H__
 
 #include "cocos2d.h"
-#include<vector>
-#include"Tower.h"
+#include <vector>
+#include "Tower.h"
 #include "BarrierAppear.h"
-#include"monster.h"
-//宏定义
+#include "monster.h"
 
+// 宏定义
 #define MENU -2
 #define PATH -1
 #define SPACE 0
 #define BARRIER 1
 #define EXISTED_TOWER 2
 
-
 #define BARRIER_1_1 0
 #define BARRIER_1_2 1
 #define BARRIER_2_1 2
 #define BARRIER_2_2 3
 #define BARRIER_4_1 4
-#define BARRIER_4_2 5 
+#define BARRIER_4_2 5
 
-extern int mapGrid[8][12];  
+extern int mapGrid[8][12];
 
-class Monster;
+class Monster;  // 前向声明
+
 class MAP_SCENE : public cocos2d::Scene
 {
 public:
-    MAP_SCENE();        // 构造函数
-    virtual ~MAP_SCENE(); // 析构函数
+    // 构造函数
+    MAP_SCENE();
+
+    // 析构函数
+    virtual ~MAP_SCENE();
 
     // 创建场景的静态方法
     static MAP_SCENE* create();
 
     // 初始化方法，子类可以重写
     bool init() override;
+    // 游戏中各个功能的成员函数
     Monster* checkMonsterClicked(Vec2 touchLocation);
     void initializeMapArray(int level);
     void onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
     void showBuildFeedback(int row, int col);
-    void clearWarningSprites();//清除提示图标
+    void clearWarningSprites();  // 清除提示图标
 
     void addTowerPreview(int row, int col);
     void deleteTower(int row, int col);
@@ -50,37 +55,41 @@ public:
     void onTowerPreviewClicked(int towerIndex, int row, int col);
     void updateordeleteTowerPreview(int row, int col);
     void handleTowerClick(int row, int col);
-    void initLevel(int level); // 初始化关卡的方法
-     void initResources();
+    void initLevel(int level);  // 初始化关卡的方法
+    void initResources();
     void setBackground(const std::string& backgroundImage);
-    //void initWaves(int allWaves); // 初始化波次的方法
     void onExitGame();
-   // void onEnterGame();
-   
     void createMouseEventListener();
+
 protected:
+    // 类的成员变量
     cocos2d::Sprite* background;  // 背景
-    bool isTowerSelected = false; // 是否已选择防御塔
-    std::vector<Node*> warningSprites; // 存储提示图片和按钮
+    bool isTowerSelected = false;  // 是否已选择防御塔
+    std::vector<Node*> warningSprites;  // 存储提示图片和按钮
+    Tower* towerArray[8][12] = { nullptr };  // 防御塔数组
     // 处理地图上不同位置的行为
     void handleMapAction(int row, int col);
-    Tower* towerArray[8][12] = { nullptr };
-    BarrierManager* barrierManager = nullptr;  // 管理障碍物
 };
-//游戏菜单类
-class GameMenu :public cocos2d::Layer
+
+class GameMenu : public cocos2d::Layer
 {
 public:
-    //构造函数
+    // 构造函数
     static cocos2d::Layer* createLayer();
+
+    // 初始化
     virtual bool init();
+
+    // 创建GameMenu对象的宏
     CREATE_FUNC(GameMenu);
+
+    // 游戏操作函数
     void start();
-    //选项
     void options();
     void showLosePopup();
     void showWinPopup();
+  
 
 };
 
-#endif // __MAP_H__
+#endif // __Game_Play_Scene_H__
