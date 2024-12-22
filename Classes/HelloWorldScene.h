2b49d76a703add47ce72bbf8f51403463de0bb24
judyyyle <judyyy2023@gmail.com>
2024-12-22 22:04:15 +0800

    删除坐标转换

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -1,27 +1,3 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #ifndef __HELLOWORLD_SCENE_H__
 #define __HELLOWORLD_SCENE_H__
 
@@ -33,27 +9,6 @@
 #define SPACE 0
 #define BARRIER 1
 #define EXISTED_TOWER 2
-
-//世界坐标与数组的转换
-struct array {
-    int row;
-    int col;
-};
-
-static Vec2 array_to_vec2(int row,int col) { //返回Vec2类型，即世界坐标
-    Vec2 vec;
-    vec.x = 64 + 128 * col;
-    vec.y = 1024 - 64 - 128 * row;
-    return vec;
-}
-
-static array vec2_to_array(Vec2 vec) { //返回array类型，即数组
-    array arr;
-    arr.row = 8 - 0.5 - static_cast<int>((vec.y / 128));
-    arr.col = static_cast<int>((vec.x / 128));
-    return arr;
-}
-
 
 class HelloWorld : public cocos2d::Scene
 {
